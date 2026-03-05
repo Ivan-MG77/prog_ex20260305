16a98afe064c6b28431a2cc1c23915fd5d482537
ivanm <ivan.m.g.1791@gmail.com>
2026-03-05 09:10:57 +0100

    feat(CentroFP): Creacion de los atributos y el metodo constructor de la clase CentroFP

--- src/CentroFP.java
+++ src/CentroFP.java
@@ -1,2 +1,13 @@
 public class CentroFP {
+
+    private Alumno[] alumnos;
+    private final int MAX_ALUMNOS;
+
+    public CentroFP(int MAX_ALUMNOS) {
+        this.MAX_ALUMNOS = MAX_ALUMNOS;
+
+        alumnos = new Alumno[MAX_ALUMNOS];
+
+    }
+
 }
